84a0fe5e067300f12698108d05c60e33059c5eec
Huy <leduchuy271@gmail.com>
2026-02-22 21:09:26 +0700

    testing api + db

--- server/db.js
+++ server/db.js
@@ -1,18 +1,28 @@
-import sqlite3 from 'sqlite3';
+import sqlite3 from "sqlite3";
 
 const sqlite = sqlite3.verbose();
-const db = new sqlite.Database('./tasks.db');
+const db = new sqlite.Database("./tasks.db", (err) => {
+  if (err) console.log("Error opening database!");
+  else console.log("DB connected!");
+});
 
 db.serialize(() => {
-    db.run(`
-        CREATE DATABASE IF NOT EXIST tasks(
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            title TEXT NOT NULL,
-            description TEXT,
-            priority TEXT default 'medium',
-            status TEXT default 'pending'
+  db.run(
+    `
+        CREATE TABLE IF NOT EXISTS tasks (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        title TEXT NOT NULL,
+        description TEXT,
+        priority TEXT DEFAULT 'medium',
+        status TEXT DEFAULT 'pending',
+        created_at TEXT DEFAULT CURRENT_TIMESTAMP
         )
-        `)
+        `,
+    (err) => {
+      if (err) console.log("Error creating DB!", err.message);
+      else console.log("DB generating successful!");
+    },
+  );
 });
 
 export default db;
--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
-import express from 'express';
-import cors from 'cors';
-// import db from './db.js';
+import express from "express";
+import cors from "cors";
+import db from "./db.js";
 
 const app = express();
 const PORT = 5000;
@@ -8,8 +8,51 @@ const PORT = 5000;
 app.use(cors());
 app.use(express.json());
 
-app.get('/test', (req, res) => {
-    res.send('Server running!');
-})
+app.get("/api/test", (req, res) => {
+  res.json({ message: "Server alive!", time: new Date().toISOString() });
+});
 
-app.listen(PORT, () => console.log(`Server running at http://localhost:${PORT}`));
+app.get("/api/tasks", (req, res) => {
+  db.all("SELECT * FROM tasks ORDER BY created_at DESC", [], (err, rows) => {
+    if (err) {
+      console.error(err.message);
+      return res.status(500).json({ error: "DB error!" });
+    }
+    res.json(rows);
+  });
+});
+
+app.post("/api/tasks", (req, res) => {
+  const {
+    title,
+    description,
+    priority = "medium",
+    status = "pending",
+  } = req.body;
+
+  if (!title) {
+    return res.status(400).json({ message: "Title is required!" });
+  }
+
+  const sql = `
+        INSERT INTO tasks (title, description, priority, status)
+        VALUES (?, ?, ?, ?)
+    `;
+
+  const params = [title, description || null, priority, status];
+
+  db.run(sql, params, function (err) {
+    if (err) {
+      console.error(err.message);
+      return res.status(500).json({ error: "Failed to create task!" });
+    }
+    return res.status(201).json({
+        id: this.lastID,
+        message: 'Task created successfully!'
+    })
+  });
+});
+
+app.listen(PORT, () =>
+  console.log(`Server running at http://localhost:${PORT}`),
+);
